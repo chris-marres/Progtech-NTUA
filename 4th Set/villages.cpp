eff031a249cbf156d5d0bd9e5649bd4d0c96b575
chris-marres <xristos.marres@gmail.com>
2022-05-18 22:41:17 +0300

    final working implementation of villages

--- 4th Set/villages.cpp
+++ 4th Set/villages.cpp
@@ -1,43 +1,45 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-struct village{
-    vector<int> connections;
-    int group = 0;
-    bool visited = false;
-};
-
-void checkEveryConnections(int group, village v, village* villages){
-    for(int i = 0; i < v.connections.size(); i++){
-        v.visited = true;
-        v.group = group;
-        if (!villages[v.connections[i]].visited) checkEveryConnections(group, villages[v.connections[i]], villages);
-    }
-}
-
-int main(){
-
-    int N, M, K;
-
-    cin >> N >> M >> K;
-
-    village* villages = new village[N+1];
-
-    for (int i = 0; i < M; i++){
-        int a, b;
-        cin >> a >> b;
-        villages[a].connections.push_back(b);
-    }
-
-    for (int i = 1; i <= N; i++){
-        if (!villages[i].visited) checkEveryConnections(i, villages[i], villages);
-    }
-
-
-    for (int i = 1; i <= N; i++){
-        cout << "Village " << i << " belongs to group " << villages[i].group << endl;
-    }
-
-    return 0;
-}
+#include <iostream>  
+#include <vector>  
+using namespace std;  
+    
+struct village{  
+    vector<int> connections;  
+    bool visited = false;  
+};  
+    
+void checkEveryConnections(village &v, village* villages){  
+    v.visited = true;  
+    for(int i = 0; i < v.connections.size(); i++){  
+        if (!villages[v.connections[i]].visited) checkEveryConnections(villages[v.connections[i]], villages);  
+    }  
+}  
+    
+int main(){  
+    
+    int N, M, K;  
+    
+    cin >> N >> M >> K;  
+    
+    village* villages = new village[N+1];  
+    
+    for (int i = 0; i < M; i++){  
+        int a, b;  
+        cin >> a >> b;  
+        villages[a].connections.push_back(b);  
+        villages[b].connections.push_back(a);  
+    }  
+    
+    int groups = 0;  
+    for (int i = 1; i <= N; i++){  
+        if (!villages[i].visited){  
+            checkEveryConnections(villages[i], villages);  
+            groups++;  
+        }  
+    }  
+    
+    int diff = groups - K;  
+    if (diff > 1) cout << diff << endl;  
+    else cout << 1 << endl;  
+    
+    return 0;  
+}  
